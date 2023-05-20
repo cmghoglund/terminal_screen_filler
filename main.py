50c22428a772d6c37557936fae6a1f274ec1c6fe
cmghoglund <cmghoglund@gmail.com>
2023-05-20 20:17:52 +0900

    Separated printing of lines into its own function.

--- main.py
+++ main.py
@@ -9,16 +9,23 @@ def clear_screen():
     os.system('cls' if os.name == 'nt' else 'clear')
 
 def fill_screen():
-    columns, lines = os.get_terminal_size()
-    for _ in range(lines):
-        # Randomly fill each line with '0' or '1'
+    columns, rows = os.get_terminal_size()
+    lines = []
+    for _ in range(rows):
+        # Randomly fill each line with '0's or '1's
         line = ''.join(str(random.randint(0, 1)) for _ in range(columns))
+        lines.append(line)
+    return lines
+
+def print_screen(lines):
+    for line in lines:
         print(line)
 
 def main():
     while True:
         clear_screen()
-        fill_screen()
+        lines = fill_screen()
+        print_screen(lines)
         print("\nPress any key to redraw screen, or 'q' to quit.\n")
         key = msvcrt.getch().decode('utf-8').lower()
         if key == 'q':
